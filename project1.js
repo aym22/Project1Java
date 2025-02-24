ae3cc14ebfd3854bcfae82f3f18db361d6858381
Ashley Moreiro <56128432+aym22@users.noreply.github.com>
2025-02-23 20:30:25 -0500

    Add files via upload
    Rename projects on Replit before downloading

---
+++ project1.js
@@ -0,0 +1,33 @@
+var name = 'Ms.Ashley';
+console.log(name);
+/// Comments
+// This outputs a variable
+
+//They do not display in the console
+var hobby = "video games";
+var age = 25;
+
+//Addition
+var a = 3.3, b = 2.5;
+var c = a + b;
+console.log(c)
+// Using Operators
+//Subraction
+var d = a - b;
+console.log(d)
+
+var e = 50%15;
+console.log(e)
+
+
+//Add Strings
+var food =               'burger';
+var drink = '         soda';
+console.log(food + drink)
+
+//Making a bio
+var firstName = "Ashley";
+var lastName = "More";
+var fullName = firstName.concat(lastName);
+
+console.log("My name is " + fullName)
